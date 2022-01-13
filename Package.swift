20f8f82622ae7d611e178ce1914be3cd9bedd9f1
Panda Rajaram <prajaram@miteksystems.com>
2022-01-13 09:32:19 -0600

    Added Source path.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "MiSnapUX"
+            name: "MiSnapUX",
+            path: "Source"
         ),
         .binaryTarget(
             name: "MiSnap",
